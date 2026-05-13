e51d45b8feeee946a3ef6ff769d68139280107f7
Paulo Vitor <pvdoederlein@gmail.com>
2026-05-12 22:15:03 -0300

    exponencial search algorithm

---
+++ exponencial_search.py
@@ -0,0 +1,29 @@
+def binary_search(nums,n,lo=0,hi=None):
+    if hi == None:
+        hi = len(nums) - 1
+
+    while lo < hi:
+        mid = int((lo+hi)/2)
+
+        if nums[mid] == n:
+            return mid
+        elif nums[mid] < n:
+            lo = mid + 1
+        else:
+            hi = mid
+    return -1
+
+def exponential_search(arr, target):
+    if arr[0] == target:
+        return 0
+    n = len(arr)
+    i = 1
+
+    while i < n and arr[i] < target:
+        i *= 2
+    
+    if arr[i] == target:
+        return i 
+    
+    binary_search(arr, target,i//2, min(i,(n - 1)))
+
